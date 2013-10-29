15bdf03698c9be63d1672ca11c38756aa784f794
Duco Dokter <dokter@w20e.com>
2013-10-29 17:02:44 +0100

    userprofile dynamic

--- djinn_profiles/urls.py
+++ djinn_profiles/urls.py
@@ -3,9 +3,8 @@ from djinn_contenttypes.views.utils import generate_model_urls
 from utils import get_userprofile_model
 
 
-UserProfile = get_userprofile_model()
-
-
 urlpatterns = patterns('',
-    (r'^profiles/', include(generate_model_urls(UserProfile))),
+    (r'^profiles/', include(generate_model_urls(
+                get_userprofile_model(),
+                name=("djinn_profiles", "userprofile")))),
 )
